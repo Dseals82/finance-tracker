ccf86c4885555628f794aba34c49ce692bd37697
Darius Seals <tristarmediagroupinc@gmail.com>
2018-07-17 13:15:56 -0400

    modified stylesheet and updated users controller.

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,10 +2,10 @@ class Stock < ApplicationRecord
   def self.new_from_lookup(ticker_symbol)
     looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
     price = strip_commas(looked_up_stock.l)
-    new(name: looked_up_stock.name, ticker: looked_up_stock.symbol,
-    last_price: price)
+    new(name: looked_up_stock.company_name, ticker: looked_up_stock.symbol,
+    last_price: looked_up_stock.latest_price)
   end
-  def self.strip_commas
+  def self.strip_commas(number)
     number.gsub(",","")
   end
 end
