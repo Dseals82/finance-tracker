915c840eb909c8a8436601cf8d326842505e17f2
Darius Seals <tristarmediagroupinc@gmail.com>
2018-08-01 23:30:07 -0400

    modified javascript files, added restrictions for stock limit and to prevent duplicate stocks

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ root to: 'welcome#index'
   get 'welcome/index'
   get 'my_portfolio', to: "users#my_portfolio"
   get 'search_stocks', to: "stocks#search"
+  resources :user_stocks, only: [:create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
